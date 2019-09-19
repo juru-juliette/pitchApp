5f9d105a3792ab9e84270caf707d502b27901c65
juru-juliette <kangabejuliette@gmail.com>
2019-09-19 19:22:35 +0200

    creating authentication

--- app/__init__.py
+++ app/__init__.py
@@ -27,9 +27,7 @@ def create_app(config_name):
     # setting config
     from .request import configure_request
     configure_request(app)
-    # configure UploadSet
-    configure_uploads(app,photos)
-
+    
 
 
     return app
