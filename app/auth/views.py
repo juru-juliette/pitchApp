a58d70e9fded823a5e55c0ce0305c647786e3437
juru-juliette <kangabejuliette@gmail.com>
2019-09-20 10:08:13 +0200

    creating function for login

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,18 @@
-from flask import render_template,redirect,url_for
+from flask import render_template,redirect,url_for,flash
 from . import auth
 from ..models import User
 from .. import db
 from .forms import LoginForm
 from flask_login import login_user,logout_user,login_required
+
+@auth.route('/login',methods=['GET','POST'])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash('Invalid username or Password')
+    title = "pitch login"
+    return render_template('auth/login.html',login_form = login_form,title=title)   
